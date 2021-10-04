95fd1b9d775b12e31139327a888d4bff4e191691
Denys Vladymyrov <link_amd@yahoo.com>
2021-10-04 13:12:59 +0300

    added tailwind and main layout.

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,28 @@
 <template>
-  test
+  <section class="flex flex-col max-w-xs p-6 items-start text-left">
+    <label for="startInput">Enter arithmetic expression</label>
+    <input type="text" id="startInput" class="form-input px-2 py-1" v-model="expression">
+  </section>
 </template>
 
 <script>
-
-
-export default {
-  name: 'App',
-}
+  export default {
+    name: 'App',
+    data() {
+      return {
+        expression: '',
+      }
+    },
+  }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
